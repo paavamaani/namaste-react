1d5c4b7c557079ba89aa2b27e621574f2e8bc5f7
Paavamaani Manchala <paavamaanimanchala@gmail.com>
2023-06-13 16:34:03 -0700

    added scripts to run parcel and fragements

--- App.js
+++ App.js
@@ -1,26 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const element = React.createElement(
-    "div",
-    {className: "parent"},
-    React.createElement(
-        "div",
-        {className: "child"},
-        [
-            React.createElement(
-                "h1",
-                {className: "heading-1"},
-                "Namaste React"
-            ),
-            React.createElement(
-                "h2",
-                {className: "heading-2"},
-                "Let's do the tasks"
-            )
-        ] 
+// Creating element using React.createElement
+
+// const element = React.createElement(
+//     "div",
+//     {className: "parent"},
+//     React.createElement(
+//         "div",
+//         {className: "child"},
+//         [
+//             React.createElement(
+//                 "h1",
+//                 {className: "heading-1"},
+//                 "Namaste React"
+//             ),
+//             React.createElement(
+//                 "h2",
+//                 {className: "heading-2"},
+//                 "Let's do the tasks"
+//             )
+//         ] 
+//     )
+// );
+
+const Heading = () => {
+    return (
+        <>
+            <h1 className='heading-1'>
+                Namaste React
+            </h1>
+            <h2 className='heading-2'>
+                Let's do it Paavamaani
+            </h2>
+        </>
     )
-);
+};
+
+const App = () => {
+    return (
+        <div className='app'>
+            <Heading />
+        </div>
+    )
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(element);
+root.render(<App />);
